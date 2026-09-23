8b334d996f28d44d3751d08e931d5ad1f0acfae4
Sakshee <98686393+Sakshee30@users.noreply.github.com>
2026-09-23 23:30:55 +0530

    foundation: wire application providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 import App from "./App";
+import { AppProviders } from "./app/providers/AppProviders";
 import "./styles.css";
 import "./feature-styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
+    <AppProviders>
       <App />
-    </HashRouter>
+    </AppProviders>
   </React.StrictMode>
 );
