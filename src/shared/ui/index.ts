f797b62f718df1fcf94338013a4b7924a496dfda
Sakshee <98686393+Sakshee30@users.noreply.github.com>
2026-09-24 01:00:30 +0530

    design-system: export owned UI primitives

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,8 @@
 export { Button } from "./Button";
+export { FormField } from "./FormField";
+export { Input } from "./Input";
+export { MetricCard } from "./MetricCard";
+export { Panel } from "./Panel";
+export { Select } from "./Select";
 export { StatusBadge } from "./Status";
 export { EmptyState, ErrorState, LoadingState, Skeleton } from "./States";
